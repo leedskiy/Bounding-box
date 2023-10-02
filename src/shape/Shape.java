a517de94925083c19fb2326127a17bf09a8de321
Illia Leedskiy <leedqvo@gmail.com>
2023-10-02 19:28:16 +0200

    add task.txt and code Shape

--- src/shape/Shape.java
+++ src/shape/Shape.java
@@ -1,4 +1,14 @@
 package shape;
 
-public class Shape {
+public abstract class Shape {
+    protected double x;
+    protected double y;
+
+    public Shape(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract double getArea();
+    public abstract boolean containsPoint(double x, double y);
 }
