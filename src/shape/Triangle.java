1f67eadae44efd68d8868b2d8d71f950f21d463d
Illia Leedskiy <leedqvo@gmail.com>
2023-10-04 18:47:38 +0200

    finish Triangle class

---
+++ src/shape/Triangle.java
@@ -0,0 +1,33 @@
+package shape;
+
+import java.lang.Math;
+import java.util.ArrayList;
+
+public class Triangle extends Shape {
+    private double sideLength;
+
+    public Triangle(double x, double y, double sideLength) {
+        super(x, y);
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.sqrt(3) / 4 * sideLength * sideLength;
+    }
+
+    public ArrayList<Double> getBoundingBox() {
+        ArrayList<Double> list1 = new ArrayList<Double>();
+        double x1 = x - sideLength/2;
+        double y1 = y - sideLength/2;
+        double x2 = x + sideLength/2;
+        double y2 = x + sideLength/2;
+        
+        list1.add(x1);
+        list1.add(y1);
+        list1.add(x2);
+        list1.add(y2);
+
+        return list1;
+    }
+}
